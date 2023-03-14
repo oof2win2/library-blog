96a1f5eaf27ddfe82753fa52398bce8d21a6fe98
oof2win2 <honza.koco44@gmail.com>
2023-03-14 20:58:05 +0000

    feat(mail): use raw SendGrid API

--- src/server/mail.ts
+++ src/server/mail.ts
@@ -1,8 +1,37 @@
 import { Book, Review, User } from "@prisma/client"
-import sendgrid from "@sendgrid/mail"
 import { env } from "@/env.mjs"
 
-sendgrid.setApiKey(env.SENDGRID_API_KEY)
+const sendMail = async (to: string, subject: string, text: string) => {
+	await fetch("https://api.sendgrid.com/v3/mail/send", {
+		method: "POST",
+		body: JSON.stringify({
+			personalizations: [
+				{
+					to: [
+						{
+							email: to,
+						},
+					],
+				},
+			],
+			from: {
+				email: env.EMAIL_USERNAME,
+			},
+			subject: subject,
+			// the email body
+			content: [
+				{
+					type: "text/plain",
+					value: text,
+				},
+			],
+		}),
+		headers: {
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${env.SENDGRID_API_KEY}`,
+		},
+	})
+}
 
 export async function sendVerificationEmail(user: User, token: string) {
 	const qs = new URLSearchParams()
@@ -12,12 +41,11 @@ export async function sendVerificationEmail(user: User, token: string) {
 		process.env.VERCEL_URL ? "https://" : "http://"
 	}${process.env.VERCEL_URL || "localhost:3000"}/user/verify?${qs}`
 
-	await sendgrid.send({
-		to: user.email,
-		from: env.EMAIL_USERNAME,
-		subject: "Bookaholic Blurbs: Verify your email",
-		text: `Hello ${user.name},\n\nPlease verify your email by clicking the following link:\n\n${verificationLink}\n\nIf you did not request this, please ignore this email.\n\nRegards,\nBookaholic Blurbs`,
-	})
+	await sendMail(
+		user.email,
+		"Bookaholic Blurbs: Verify your email",
+		`Hello ${user.name},\n\nPlease verify your email by clicking the following link:\n\n${verificationLink}\n\nIf you did not request this, please ignore this email.\n\nRegards,\nBookaholic Blurbs`
+	)
 }
 
 export function notifyNewReview(
@@ -26,17 +54,14 @@ export function notifyNewReview(
 	user: User,
 	admins: User[]
 ) {
-	const emails = admins.map((admin) => {
+	for (const admin of admins) {
 		const text = `Hello ${admin.name},\n\n${user.name} has just reviewed ${book.title}.\n\n${review.reviewText}\n\nRegards,\nBookaholic Blurbs`
-		return {
-			to: admin.email,
-			from: env.EMAIL_USERNAME,
-			subject: `Bookaholic Blurbs: New review for ${book.title}`,
-			text,
-		}
-	})
-
-	// await sendgrid.sendMultiple(emails)
+		sendMail(
+			admin.email,
+			`Bookaholic Blurbs: New review for ${book.title}`,
+			text
+		)
+	}
 }
 
 export async function sendPasswordReset(user: User, token: string) {
@@ -46,10 +71,6 @@ export async function sendPasswordReset(user: User, token: string) {
 	const resetLink = `${process.env.VERCEL_URL ? "https://" : "http://"}${
 		process.env.VERCEL_URL || "localhost:3000"
 	}/user/passwordreset/new?${qs}`
-	await sendgrid.send({
-		to: user.email,
-		from: env.EMAIL_USERNAME,
-		subject: "Bookaholic Blurbs: Reset your password",
-		text: `Hello ${user.name},\n\nPlease reset your password by clicking the following link:\n\n${resetLink}\n\nIf you did not request this, please ignore this email.\n\nRegards,\nBookaholic Blurbs`,
-	})
+	const text = `Hello ${user.name},\n\nPlease reset your password by clicking the following link:\n\n${resetLink}\n\nIf you did not request this, please ignore this email.\n\nRegards,\nBookaholic Blurbs`
+	sendMail(user.email, "Bookaholic Blurbs: Reset your password", text)
 }
