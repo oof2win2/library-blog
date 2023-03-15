71dd9dc4d342d20fdcd3cc74717bb81fd196e5b7
oof2win2 <honza.koco44@gmail.com>
2023-03-15 08:11:44 +0000

    fix(trpc): set auth cookie

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -2,6 +2,7 @@ import { fetchRequestHandler } from "@trpc/server/adapters/fetch"
 import { NextFetchEvent, NextRequest } from "next/server"
 import { appRouter } from "@/server/api/root"
 import { createContext } from "@/server/api/context"
+import { env } from "@/env.mjs"
 
 // export API handler
 export default async function handler(
@@ -18,6 +19,27 @@ export default async function handler(
 				console.error(
 					`❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
 				)
+			}
+		},
+		responseMeta: ({ ctx }) => {
+			// if there is no context, we don't do anything
+			if (!ctx) return {}
+			const cookie = ctx.responseCookies.get(env.COOKIE_NAME)
+			if (!cookie) return {}
+			return {
+				headers: {
+					"Set-Cookie": `${cookie.name}=${cookie.value};${
+						cookie.httpOnly ? " HttpOnly" : ""
+					};${cookie.secure ? " Secure" : ""};${
+						cookie.sameSite ? ` SameSite=${cookie.sameSite}` : ""
+					};${cookie.path ? ` Path=${cookie.path}` : ""};${
+						cookie.domain ? ` Domain=${cookie.domain}` : ""
+					};${
+						cookie.expires
+							? ` Expires=${cookie.expires.toUTCString()}`
+							: ""
+					}`,
+				},
 			}
 		},
 	})
